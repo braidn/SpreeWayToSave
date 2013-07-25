47d3c6d2472af40c4c9cccf0d34a51eff2befcfb
Braden <braden.douglass@gmail.com>
2013-07-25 10:14:44 -0400

    Users can now be associated with having many cards

--- spec/models/spree/cc_have_a_user_id_spec.rb
+++ spec/models/spree/cc_have_a_user_id_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
-describe 'Credit Cards' do
+describe 'spree_credit_cards' do
 	let(:user) { create(:user) }
 	let(:credit_card) { create(:credit_card, gateway_payment_profile_id: 'Gateway1', user_id: user.id) }
 
-	it 'are associated to a user' do
-		expect(user.id).to eq(credit_card.user_id)
-		credit_card.should respond_to(:user_id) 
+	context 'when a user' do
+		it 'is able to associate themselves to one card' do
+			expect(user.id).to eq(credit_card.user_id)
+			credit_card.should respond_to(:user_id) 
+		end
+		it 'is able to save more than one card' do
+			card1 = credit_card #love you lazy loading
+			card2 = create(:credit_card, gateway_payment_profile_id: 'Gateway1', user_id: user.id)
+			expect(user.credit_cards.count).to eq(2)
+		end
 	end
 end
